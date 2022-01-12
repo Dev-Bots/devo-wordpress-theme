c5a0e1d812ec23a1f255ff2f04eb9493403fbd71
Tinsae <itistinsae@gmail.com>
2022-01-12 21:32:07 +0200

    Update header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    <link href="<?php bloginfo('template_url') ?>/assets/css/bootstrap.min.css" rel="stylesheet" >
+    <title><?php bloginfo('name')?></title>
+    <?php wp_head() ?>
+</head>
+<body <?php body_class()?>>
+<header class="site-header">
+<h1>   <a href="<?php echo home_url()?>"><?php bloginfo('name')?></a>  </h1>
+<h4><?php bloginfo('description')?></h4>
+
+
+
+
+</header>
+
+<?php get_template_part('template-parts/navigation/navigation', 'primary'); ?>
+<?php get_template_part('template-parts/header/header', 'image'); ?>
