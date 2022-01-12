5d0af9d8eb920f144027e796fa99cb14c53d2efd
Tinsae <itistinsae@gmail.com>
2022-01-12 21:49:31 +0200

    update navigation.php

---
+++ template-parts/navigation/navigation-primary.php
@@ -0,0 +1,22 @@
+<?php if (has_nav_menu('top-menu')) : ?>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <?php wp_nav_menu(array(
+      'theme_location'    => 'top-menu',
+      'depth'             => 4,
+      'container'         => 'div',
+      'container_class'   => 'collapse navbar-collapse',
+      'container_id'      => 'bs-example-navbar-collapse-1',
+      'menu_class'        => 'nav navbar-nav',
+      'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+    )); ?>
+
+  </div>
+</nav>
+
+<?php endif ?>
