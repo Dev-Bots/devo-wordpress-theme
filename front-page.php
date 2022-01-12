a3316b95569782c0a7753600b3e17b9dc2e2521e
Meron Mesay <it.meron.mesay@gmail.com>
2022-01-12 20:02:36 +0300

    modified front page

---
+++ front-page.php
@@ -0,0 +1,76 @@
+<?php get_header(); ?>
+
+<div >
+
+    <div class="jumbotron jumbotron-fluid">
+  <!-- <div class="container p-5">
+    <h1>About this theme</h1>      
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta animi aperiam incidunt sed beatae cumque architecto corrupti veniam alias molestias error totam dolorum odit illo dolore quam, at distinctio nobis.</p>
+  </div> -->
+
+ 
+    <div class="container">
+        <div class="row">
+        <div>
+            <h1 style="color: #777;">About this theme</h1>      
+            <p style="color: #777;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta animi aperiam incidunt sed beatae cumque architecto corrupti veniam alias molestias error totam dolorum odit illo dolore quam, at distinctio nobis.</p>
+        </div>
+       
+        </div>
+
+</div>
+
+<?php get_template_part('template-parts/services/service', 'list'); ?>
+
+<div class='testimonial-container mx-auto mt-5 col-md-10 mt-100'>
+    <div class="header">
+        <div class="title">Opinions Matter</div>
+        <p><small class="text-muted">We are a homegrown, globally connected, <br /><u>Experimental</u> HUB that prides itself on its <u>Creativity</u><br /> and <u>Innovation.</u> </small></p>
+    </div>
+    <div class="row" style="justify-content: center">
+        <div class="card col-md-3 mt-100">
+            <div class="card-content">
+                <div class="card-body p-0">
+                    <div class="profile"> <img src="https://www.macmillandictionary.com/external/slideshow/full/Black_full.png"> </div>
+                    <div class="card-title"> Jack Gates<br /> <small>CEO Of ABCDE</small> </div>
+                    <div class="card-subtitle">
+                        <p> <small class="text-muted"> I really enjoyed working with Devbots. </small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-md-3 mt-100">
+            <div class="card-content">
+                <div class="card-body p-0">
+                    <div class="profile"> <img src="https://www.macmillandictionary.com/external/slideshow/full/Black_full.png"> </div>
+                    <div class="card-title"> Jack Gates<br /> <small>CEO Of ABCDE</small> </div>
+                    <div class="card-subtitle">
+                        <p> <small class="text-muted"> I really enjoyed working with Devbots. </small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-md-3 mt-100">
+            <div class="card-content">
+                <div class="card-body p-0">
+                    <div class="profile"> <img src="https://www.macmillandictionary.com/external/slideshow/full/Black_full.png"> </div>
+                    <div class="card-title"> Jack Gates<br /> <small>CEO Of ABCDE</small> </div>
+                    <div class="card-subtitle">
+                        <p> <small class="text-muted"> I really enjoyed working with Devbots. </small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ft">
+        <div class="testimonial-btn btn btn-outline">More About Us</div>
+    </div>
+</div>
+
+
+
+</div>
+
+
+
+<?php get_footer(); ?>
