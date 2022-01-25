578f74f7cc810c3bfc000c57613e7c4c3b2074d0
Tinsae <itistinsae@gmail.com>
2022-01-25 19:05:43 +0200

    Update widgets.php

---
+++ inc/widgets.php
@@ -0,0 +1,19 @@
+<?php
+// Adding wigets 
+function devo_theme_sidebar() {
+    register_sidebar( array(
+        'name' => ( 'Sidebar', 'devo' ),
+        'id'   => 'sidebar1',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="rounded">',
+        'after_title'   => '</h2>',
+    ) );
+    register_sidebar( array(
+        'name' => ( 'Footer Ba 2', 'devo' ),
+        'id'   => 'footer_bar1',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="rounded">',
+        'after_title'   => '</h2>',
+    ) );
