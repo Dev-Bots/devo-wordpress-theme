ff78ed2167ac18aa10404e5e41ce645e6f67ff03
Tinsae <itistinsae@gmail.com>
2022-01-25 19:52:08 +0200

    Update widgets.php

--- inc/widgets.php
+++ inc/widgets.php
@@ -17,3 +17,11 @@ function devo_theme_sidebar() {
         'before_title'  => '<h2 class="rounded">',
         'after_title'   => '</h2>',
     ) );
+    register_sidebar( array(
+        'name' => ( 'Footer Bar 2', 'devo' ),
+        'id'   => 'footer_bar2',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="rounded">',
+        'after_title'   => '</h2>',
+    ) );   
